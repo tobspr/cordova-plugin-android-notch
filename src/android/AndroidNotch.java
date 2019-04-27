3b139e7a0a2d98824bb153ef39e119bebfbe8632
tobspr <tobias.springer1@googlemail.com>
2019-04-27 10:10:22 +0200

    Compilatuion fixes

--- src/android/AndroidNotch.java
+++ src/android/AndroidNotch.java
@@ -3,7 +3,20 @@
 
 package com.tobspr.androidnotch;
 
+import android.app.Activity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import java.util.Arrays;
 
 
 public class StatusBar extends CordovaPlugin {
@@ -22,6 +35,29 @@ public class StatusBar extends CordovaPlugin {
     public boolean execute(final String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
         final Activity activity = this.cordova.getActivity();
         final Window window = activity.getWindow();
+
+
+        if ("getInsetsTop".equals(action)) {
+            callbackContext.sendPluginResult(35);
+            return true;
+        }
+        
+        if ("getInsetsRight".equals(action)) {
+            callbackContext.sendPluginResult(36);
+            return true;
+        }
+
+        if ("getInsetsBottom".equals(action)) {
+            callbackContext.sendPluginResult(37);
+            return true;
+        }
+
+        if ("getInsetsLeft".equals(action)) {
+            callbackContext.sendPluginResult(38);
+            return true;
+        }
+
+
         return false;
     }
 
